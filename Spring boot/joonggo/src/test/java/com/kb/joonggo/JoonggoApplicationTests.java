ea2f65a8bf7ce0f070f17957c2668558254d262b
ofosis <dldossla@naver.com>
2023-11-15 14:19:27 +0900

    +Spring boot pom.xml추가

---
+++ Spring boot/joonggo/src/test/java/com/kb/joonggo/JoonggoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kb.joonggo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoonggoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
