6eaa0eb0773c10f5a33251a58988469b64108829
promise1a <promise1a@naver.com>
2023-11-22 10:08:37 +0900

    임시 페이징(3*1)

--- Spring boot/joonggo/src/main/java/com/kb/joonggo/Tradeboard/TradeboardController.java
+++ Spring boot/joonggo/src/main/java/com/kb/joonggo/Tradeboard/TradeboardController.java
@@ -21,14 +21,14 @@ public class TradeboardController {
         try {
             model.addAttribute("BuypageNum", BuypageNum);
 
-            BuypageNum = (BuypageNum - 1) * 5;
+            BuypageNum = (BuypageNum - 1) * 3;
             List<TradeboardDTO> Buylist = tradeboardRepository.Buylist(BuypageNum);
             int countBuyRow = tradeboardRepository.countBuyRow();
 
             model.addAttribute("Buylist", Buylist);
             model.addAttribute("countBuyRow", countBuyRow);
 
-            int countBuyPage = (countBuyRow / 5) + ((countBuyRow % 5 > 0) ? 1 : 0);
+            int countBuyPage = (countBuyRow / 3) + ((countBuyRow % 3 > 0) ? 1 : 0);
             model.addAttribute("countBuyPage", countBuyPage);
         }
         catch (Exception e){
@@ -44,14 +44,14 @@ public class TradeboardController {
         try {
             model.addAttribute("SellpageNum", SellpageNum);
 
-            SellpageNum = (SellpageNum - 1) * 5;
+            SellpageNum = (SellpageNum - 1) * 3;
             List<TradeboardDTO> Selllist = tradeboardRepository.Selllist(SellpageNum);
             int countSellRow = tradeboardRepository.countSellRow();
 
             model.addAttribute("Selllist", Selllist);
             model.addAttribute("countSellRow", countSellRow);
 
-            int countSellPage = (countSellRow / 5) + ((countSellRow % 5 > 0) ? 1 : 0);
+            int countSellPage = (countSellRow / 3) + ((countSellRow % 3 > 0) ? 1 : 0);
             model.addAttribute("countSellPage", countSellPage);
         }
         catch (Exception e){
