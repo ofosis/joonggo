cabee0aeb6206a2e8d361892614cac2a6e6f4af7
jinjin555 <sungjin9945@naver.com>
2023-11-30 15:07:30 +0900

    실패

--- Spring boot/joonggo/src/main/java/com/kb/joonggo/Tradeboard/TradeboardController.java
+++ Spring boot/joonggo/src/main/java/com/kb/joonggo/Tradeboard/TradeboardController.java
@@ -292,7 +292,6 @@ public class TradeboardController {
     public ResponseEntity<TradeboardReq> getTradeboardInfo(@RequestParam(name = "tb_idx", required = true) Integer tb_idx) {
         if (tb_idx != null) {
             TradeboardReq tradeboardReq = tradeboardRepository.selectRow2(tb_idx);
-            System.out.println(tradeboardReq);
             if (tradeboardReq.getTb_date() == null) {
                 tradeboardReq.setTb_date(new Date());
             }
