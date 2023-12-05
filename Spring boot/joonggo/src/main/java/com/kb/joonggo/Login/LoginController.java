53bf02900894758f602b39608343b55fc51502ad
ofosis <dldossla@naver.com>
2023-12-05 14:36:40 +0900

    로그인 세션유지 설정

--- Spring boot/joonggo/src/main/java/com/kb/joonggo/Login/LoginController.java
+++ Spring boot/joonggo/src/main/java/com/kb/joonggo/Login/LoginController.java
@@ -1,7 +1,11 @@
 package com.kb.joonggo.Login;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -22,21 +26,36 @@ public class LoginController {
 
     @GetMapping("loginnig")
     @ResponseBody
-    public String loginnig(String mbr_id, String mbr_pwd,String mbr_nick) {
+    public String loginnig(String mbr_id, String mbr_pwd, String mbr_nick, HttpSession session, Model model) {
         System.out.println("login 컨트롤러들어옴");
-        System.out.println("입력된 mbr_id : "+mbr_id);
-        System.out.println("입력된 mbr_pwd : "+mbr_pwd);
+        System.out.println("입력된 mbr_id : " + mbr_id);
+        System.out.println("입력된 mbr_pwd : " + mbr_pwd);
 
-        if(mbr_id.equals(""))
-            return  "아이디를 입력해주세요.";
+        if (mbr_id.equals(""))
+            return "아이디를 입력해주세요.";
         else if (mbr_pwd.equals(""))
-            return  "비밀번호을 입력해주세요.";
+            return "비밀번호을 입력해주세요.";
 
         Login DBLogin = loginRepository.login(mbr_id, mbr_pwd);
 
-        if(DBLogin == null){
+        if (DBLogin == null) {
             System.out.println("-----DB값 없을 때-----");
-            return   "아이디 또는 비밀번호를 다시 확인해주세요.";
+            return "아이디 또는 비밀번호를 다시 확인해주세요.";
+        }
+
+        System.out.println("---DB불러왔음---");
+        mbr_nick = DBLogin.getMbr_nick();
+        mbr_id = DBLogin.getMbr_id();
+        mbr_pwd = DBLogin.getMbr_pwd();
+        // 세션에 사용자 정보 저장
+        session.setAttribute("loggedInUser", DBLogin);
+
+        // 모델에 사용자 정보 담기
+        model.addAttribute("userinfo", DBLogin);
+
+        if (DBLogin == null) {
+            System.out.println("-----DB값 없을 때-----");
+            return "아이디 또는 비밀번호를 다시 확인해주세요.";
         }
 
         System.out.println("---DB불러왔음---");
@@ -44,15 +63,30 @@ public class LoginController {
         mbr_id = DBLogin.getMbr_id();
         mbr_pwd = DBLogin.getMbr_pwd();
 
-        if(DBLogin != null) {
-            System.out.println("-----DB값 있을 때-----");
-            System.out.println("mbr_id : "+mbr_id);
-            System.out.println("mbr_pwd : "+mbr_pwd);
-            System.out.println("닉네임 : "+mbr_nick);
-            System.out.println("로그인 성공");
-            return mbr_nick+"님 환영합니다";
-        }
-        else { return "뭔가오류남"+mbr_id; }
-    }
+        // 세션에 사용자 정보 저장
+        session.setAttribute("loggedInUser", DBLogin);
 
+        // 모델에 사용자 정보 담기
+        model.addAttribute("userinfo", DBLogin);
+
+        if (DBLogin != null) {
+            System.out.println("-----DB값 있을 때-----");
+            System.out.println("mbr_id: " + mbr_id);
+            System.out.println("mbr_pwd: " + mbr_pwd);
+            System.out.println("닉네임: " + mbr_nick);
+            System.out.println("로그인 성공");
+
+            // DBLogin 객체를 JSON으로 변환하여 반환
+            ObjectMapper mapper = new ObjectMapper();
+            try {
+                String jsonDBLogin = mapper.writeValueAsString(DBLogin);
+                return jsonDBLogin;
+            } catch (JsonProcessingException e) {
+                e.printStackTrace();
+                return "로그인 실패";
+            }
+        } else {
+            return "로그인 실패";
+        }
+    }
 }
