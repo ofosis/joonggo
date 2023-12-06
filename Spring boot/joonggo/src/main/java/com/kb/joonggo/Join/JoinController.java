9276e17d292ba3c06b1d861b3dce9507bd5d508d
jinjin555 <sungjin9945@naver.com>
2023-12-06 12:23:53 +0900

    더미

--- Spring boot/joonggo/src/main/java/com/kb/joonggo/Join/JoinController.java
+++ Spring boot/joonggo/src/main/java/com/kb/joonggo/Join/JoinController.java
@@ -23,47 +23,49 @@ public class JoinController {
         return "Join/joinpage";
     }
 
+    @GetMapping("check_id")
+    @ResponseBody
+    public String check_id(String mbr_id) {
+        System.out.println("check_id 컨트롤러 들어옴");
+        System.out.println("입력된 mbr_id : "+mbr_id);
+
+        if(mbr_id.equals(""))
+            return  "비어 있을 수 없습니다.";
+
+        Join DBjoin = joinRepository.check_id(mbr_id);
+
+        if(DBjoin != null) {
+            System.out.println("중복일 때 id : "+mbr_id);
+            return  mbr_id+"는 중복된 아이디 입니다.";
+        }
+        else {
+            System.out.println("중복 아닐 때 id :"+mbr_id);
+            return "사용 가능한 아이디 입니다";
+        }
+    }
+
     @PostMapping("joininsert")
     public String joininsert(JoinReq joinReq) {
-        System.out.println("joininsert 컨트롤러들어옴");
+        System.out.println("joininsert 컨트롤러 들어옴");
 
         System.out.println(joinReq);
 
-
         Join join = Join.builder()
-                        .mbr_nick(joinReq.getMbr_nick())
-                        .mbr_id(joinReq.getMbr_id())
-                        .mbr_pwd(joinReq.getMbr_pwd())
-                        .mbr_name(joinReq.getMbr_name())
-                        .mbr_contact(joinReq.getMbr_contact())
-                        .mbr_email(joinReq.getMbr_email())
-                        .mbr_addr(joinReq.getMbr_addr())
-                        .build();
+                .mbr_nick(joinReq.getMbr_nick())
+                .mbr_id(joinReq.getMbr_id())
+                .mbr_pwd(joinReq.getMbr_pwd())
+                .mbr_name(joinReq.getMbr_name())
+                .mbr_contact(joinReq.getMbr_contact())
+                .mbr_email(joinReq.getMbr_email())
+                .mbr_addr(joinReq.getMbr_addr())
+                .build();
         joinRepository.insert(join);
 
-        System.out.println("회원가입완료");
+
+        System.out.println("회원가입 완료");
 
         return "redirect:/Login/loginpage";
     }
-    @GetMapping("check_id")
-    @ResponseBody
-    public String check_id(String id) {
-        System.out.println("check_id 컨트롤러들어옴");
-
-        Join DBjoin = joinRepository.check_id(id);
-
-        if(DBjoin != null) {
-            System.out.println("id :"+id);
-            return  "fail";
-        }
-        else {
-            System.out.println("id :"+id);
-            return "suc";
-        }
-
-
-    }
-
 }
 
 
